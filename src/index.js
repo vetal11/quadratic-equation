1d8e62076b60fe4e214940ee5f6b1b740b73350d
vetal11 <vetalblack@list.ru>
2018-09-22 18:18:57 +0300

    !

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  
+  equation = equation.replace(/\s/g,'')
+ 
+  let a = equation.split('*')[0];
+  let b = equation.split('*')[1].split('x^2')[1];
+  let c = equation.split('*')[2].split('x')[1];
+  
+  let D = b * b - 4 * a * c;
+  let x1 = (-b + Math.sqrt(D)) / (2 * a);
+  let x2 = (-b - Math.sqrt(D)) / (2 * a);
+  
+  if (a > 0) {
+    let temp = x1
+    x1 = x2
+    x2 = temp
+  }  
+
+  return [Math.round(x1), Math.round(x2)];    
 }
